e0442c8ffacc66296f4fd6b618dfd4b2bc711c5c
juliusmwass <juliusmwass100@gmail.com>
2026-01-23 07:06:52 +0300

    Added auth.controler file for request handling

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import { registerOwnerService, loginService } from "../services/auth.service.js";
+
+export const registerOwner = async (req, res) => {
+  try {
+    const result = await registerOwnerService(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const result = await loginService(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(401).json({ message: err.message });
+  }
+};

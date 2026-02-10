8b77d3b110fdfd01cb058a43e690c5742ee7c784
juliusmwass <juliusmwass100@gmail.com>
2026-02-10 06:09:45 +0300

    Added a branches controller page

---
+++ backend/src/controllers/branch.controller.js
@@ -0,0 +1,23 @@
+import prisma from "../lib/prisma.js";
+
+export const getBranches = async (req, res) => {
+  try {
+    const branches = await prisma.branch.findMany({
+      orderBy: { createdAt: 'desc' }
+    });
+    res.json({ data: branches });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const createBranch = async (req, res) => {
+  try {
+    const newBranch = await prisma.branch.create({
+      data: req.body
+    });
+    res.status(201).json(newBranch);
+  } catch (error) {
+    res.status(400).json({ message: "Code must be unique or data is missing" });
+  }
+};

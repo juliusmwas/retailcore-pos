d2bb8714282ad880c50fab59ed1a77ce36b46ec8
juliusmwass <juliusmwass100@gmail.com>
2026-03-18 19:16:08 +0300

    Added a fuction to allow admin edit and update branch details from frontend to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: "http://localhost:5173", 
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
@@ -22,6 +22,7 @@ app.use(cors({
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }

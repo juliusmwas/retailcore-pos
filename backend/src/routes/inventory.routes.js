b91663e768bf364fb844db91e5d1c054e773e5cf
juliusmwass <juliusmwass100@gmail.com>
2026-04-06 14:10:35 +0300

    Updated the routes js

--- backend/src/routes/inventory.routes.js
+++ backend/src/routes/inventory.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { getBranchInventory } from "../controllers/inventory.controller.js";
+import {
+  getBranchInventory,
+  adjustStock,
+} from "../controllers/inventory.controller.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();

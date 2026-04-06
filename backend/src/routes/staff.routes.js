cf8effd65d0f55bb97d8fc062525aafe1e0c913b
juliusmwass <juliusmwass100@gmail.com>
2026-04-06 17:21:59 +0300

    Updated the staff routing to handle password updates

--- backend/src/routes/staff.routes.js
+++ backend/src/routes/staff.routes.js
@@ -5,11 +5,13 @@ import {
   updateStaff,
 } from "../controllers/staff.controller.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
+import { updateSettings } from "../controllers/staff.controller.js";
 
 const router = express.Router();
 
 // Only logged in users (Admins/Owners) should manage staff
 router.post("/", verifyToken, addStaff);
+router.put("/settings/update", verifyToken, updateSettings);
 router.get("/", verifyToken, getStaff);
 
 router.put("/:id", verifyToken, updateStaff);

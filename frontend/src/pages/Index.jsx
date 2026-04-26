86be9e75cb74a25b07db0adf67d1978422f75544
juliusmwass <juliusmwass100@gmail.com>
2026-04-26 21:17:39 +0300

    Updated the Index page

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,28 +1,49 @@
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 
 function Index() {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 overflow-hidden">
-      {/* With a cropped image, a simple flex container handles perfect centering */}
+    <div className="bg-blue-200">
+      {/*Navbar Section*/}
+      <div className="flex  justify-between p-4 text-center ">
+        {/*logo */}
+        <div className="logo"></div>
+
+        {/*Nav-links*/}
+        <div className="nav-links flex gap-6 list-none">
+          <li>Features</li>
+          <li>About Us</li>
+          <li>How it Works</li>
+          <li>Contact Us</li>
+        </div>
+
+        {/*Nav-btn*/}
+        <div className="btn flex gap-3 text-center">
+          <button className="login border border-blue-600 py-2 px-4 rounded-lg text-blue-600 font-medium cursor-pointer">
+            Login
+          </button>
+          <button className="border border-blue-600 py-2 px-4 rounded-lg text-blue-600 font-medium cursor-pointer">
+            Register
+          </button>
+        </div>
+      </div>
+
+      {/*Hero Section */}
+    </div>
+    /*<div className="min-h-screen flex items-center justify-center bg-gray-100 overflow-hidden">
       <div className="flex flex-col items-center animate-in fade-in duration-1000">
-        
         <img 
           src="/public/lg.png" 
           alt="Logo" 
           className="w-96 h-auto block" 
         />
-        
-        {/* With the whitespace gone, you only need a small standard margin (mt-4) */}
         <button
           onClick={() => navigate("/login")}
           className="mt-15 px-6 py-3 bg-blue-600 font-semibold cursor-pointer text-white rounded-lg text-lg hover:bg-blue-700  transition-all shadow-lg "
-        >
-          Login
-        </button>
+        >Login</button>
       </div>
-    </div>
+    </div>*/
   );
 }
 
